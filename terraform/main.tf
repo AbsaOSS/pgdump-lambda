a0a401b8669908e3dfc48b935171a2e4023ccea6
Oto Macenauer <112095903+oto-macenauer-absa@users.noreply.github.com>
2025-07-22 15:53:44 +0200

    Update pgdump lambda for Postgres 17 (#3)

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,11 +16,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.2.0"
     }
   }
 
